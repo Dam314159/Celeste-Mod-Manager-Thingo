32e1eed369d832c6623964087a27554719cd78d6
InsertTextHereInsertTextHere <damien.leongpm@gmail.com>
2025-01-05 23:20:51 +0800

    Remove ModAttribute class implementation from modManagerV2.cpp

--- ModManagerV2/modManagerV2.cpp
+++ ModManagerV2/modManagerV2.cpp
@@ -7,25 +7,6 @@
 
 // Custom datatype to store mod attributes
 #include "ModAttribute.h"
-public:
-bool isFavourite;
-std::set<std::string> dependencies;
-bool isEnabled;
-
-ModAttribute(bool isFavourite = false, std::set<std::string> dependencies = {}, bool isEnabled = false)
-    : isFavourite(isFavourite), dependencies(dependencies), isEnabled(isEnabled) {}
-
-void print(int indent) const {
-    std::string spaces(indent, ' ');
-    std::cout << spaces << "Is Favourite: " << (isFavourite ? "True" : "False") << '\n';
-    std::cout << spaces << "Dependencies:\n";
-    for (const auto &dependency : dependencies) {
-        std::cout << spaces << spaces << dependency << '\n';
-    }
-    std::cout << spaces << "Is Enabled: " << (isEnabled ? "True" : "False") << "\n\n";
-}
-}
-;
 
 // Optional
 // ↳ Handle mod options order, updater blacklist
