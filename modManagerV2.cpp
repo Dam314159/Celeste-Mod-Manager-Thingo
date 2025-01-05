543569f6a748438c057a3c873c1732f9164a6775
InsertTextHereInsertTextHere <damien.leongpm@gmail.com>
2025-01-05 16:13:49 +0800

    WIP

---
+++ modManagerV2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "miniz.h" //Credit to richgel999 on GitHub for the miniz library
+
+/*
+Setup
+↳
+*/
+
+/*
+Main Menu
+↳ Display Text
+    ↳ Welcome message
+    ↳ Options
+    ↳ Current mods folder
+    ↳ Change mods folder option
+    ↳ Prompt input
+↳ Wait for user input (A)
+↳ Validate user input
+    ↳ If correct go to the corresponding option (<Change Mods Folder> | )
+    ↳ Else goto (A)
+*/
+
+/*
+Change Mods Folder
+↳ Display Text
+    ↳ How to find the correct folder
+    ↳ Prompt Input
+↳ Wait for user input (A)
+↳ Validate and sanitise user input
+    ↳ If correct goto <Main Menu>
+    ↳ Else goto(A)
+*/
+
+/*
+Edit Favourite
+*/
